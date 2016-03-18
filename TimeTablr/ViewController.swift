74e4fe431cc9ae8d7e1a7cea6e540d2760c4844e
Akbarzadeh, Pasha <18akbape@administrators-imac.local>
2016-03-18 11:09:34 +1100

    Initial Commit

---
+++ TimeTablr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTablr
+//
+//  Created by Akbarzadeh, Pasha on 18/03/2016.
+//  Copyright © 2016 Akbarzadeh, Pasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TimeTablrTests/TimeTablrTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeTablrTests.swift
+//  TimeTablrTests
+//
+//  Created by Akbarzadeh, Pasha on 18/03/2016.
+//  Copyright © 2016 Akbarzadeh, Pasha. All rights reserved.
+//
+
+import XCTest
+@testable import TimeTablr
+
+class TimeTablrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
